5eb2bfd25e687ed1285c4f05fe2c837a02de75b8
Vichaya Sirisanthana <vsirisanthana@proteus-tech.com>
2013-01-21 13:52:12 +0700

    Add nearby coffee places in map

--- webapp/static/js/controller.js
+++ webapp/static/js/controller.js
@@ -1,4 +1,8 @@
 function AppCtrl($scope) {
+
+    $scope.apiKey = 'AIzaSyBf9_6dgjtdzdn1lqYPWn209VPUN6Kv040';
+    $scope.tableId = '1R55mklI2c8q7uhC0QxaGKZXGWN-KWfy9NMtlAHo';
+
     $scope.x = 100;
     $scope.r = Math.pow(3, -1/2);
     $scope.s = 1/5;
@@ -25,4 +29,58 @@ function AppCtrl($scope) {
 //    google.maps.event.addListener($scope.marker, 'mouseout', function() {
 //        $scope.marker.setAnimation(google.maps.Animation.BOUNCE);
 //    });
+
+//    $http.get('https://www.googleapis.com/fusiontables/v1/query?sql=SELECT%20*%20FROM%201R55mklI2c8q7uhC0QxaGKZXGWN-KWfy9NMtlAHo&key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ')
+//        .success(function(data, status, headers, config) {
+//            console.log('YES', data);
+//        })
+//        .error(function(data, status, headers, config) {
+//            console.log('SUCK', data);
+//        });
+
+
+//    $scope.roastniyom = new google.maps.Marker({
+//        position: new google.maps.LatLng(18.791880, 98.970211),
+//        title: 'Roastniyom Coffee'
+//    });
+//    $scope.roastniyom.setMap($scope.map);
+//
+//    $scope.mingmitr = new google.maps.Marker({
+//        position: new google.maps.LatLng(18.795460, 98.970812),
+//        title: 'Mingmitr Coffee'
+//    });
+//    $scope.mingmitr.setMap($scope.map);
+
+//    $scope.layer = new google.maps.FusionTablesLayer({
+//        query: {
+//            select: 'Location',
+//            from: $scope.tableId
+//            where: 'ST_INTERSECTS(Location, CIRCLE(LATLNG(18.793589, 98.972349), 300))'
+//        }
+//    });
+
+//    google.maps.event.addListener($scope.layer, 'click', function(e) {
+
+        // Change the content of the InfoWindow
+//        e.infoWindowHtml = e.row['Text'].value;
+//    });
+
+//    $scope.layer.setMap($scope.map);
+    $scope.places = [];
+
+    $.get('https://www.googleapis.com/fusiontables/v1/query?sql=SELECT%20Name,Location%20FROM%20' + $scope.tableId +
+        '&key=' + $scope.apiKey, function(data, textStatus, jqXHR) {
+            $scope.places = data.rows;
+            angular.forEach($scope.places, function(place) {
+                var latLng = place[1].split(','),
+                    lat = parseFloat(latLng[0]),
+                    lng = parseFloat(latLng[1]),
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(lat, lng),
+                        title: place[0],
+                        icon: '/static/images/1358717663_kteatime.png'
+                    });
+                marker.setMap($scope.map);
+            });
+        });
 }
